187b55608cd6e1183966ea7bd2d0d3c89876c230
Michael Morale <michaelmorale1@gmail.com>
2025-12-21 08:08:27 -0600

    Initial Commit

---
+++ QuickShot/QuickShotApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickShotApp.swift
+//  QuickShot
+//
+//  Created by Michael Morale on 12/21/25.
+//
+
+import SwiftUI
+
+@main
+struct QuickShotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
